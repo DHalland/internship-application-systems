6c2502dda40490898b157acc9ec0e268a975cd9a
Dylan Halland <dylanhalland@gmail.com>
2020-04-17 13:34:45 -0700

    successfully opened socket fd

--- ping.c
+++ ping.c
@@ -19,7 +19,7 @@
 #include <arpa/inet.h>
 #include <time.h>
 
-#include "cloudflare.h"
+#include "ping.h"
 
 int ping = 1; // global var used to exit main ping loop after Ctrl-C interrupt
 
@@ -60,21 +60,63 @@ struct addrinfo getIPAddress(char *dest) {
 }
 
 void pingIPAddress(char *dest) {
-	int i, byte;
+	Packet pkt;
+	int i, byte, sent, received, skt, src_len;
+	double total_time;
+	long double rtt;
+
 	struct addrinfo res;
 	struct sockaddr_in *addr;
+	struct sockaddr source;
+	struct timespec t0, t1;
+
 	int icmp_seq = 1;
 
 	signal(SIGINT, stopPinging);
 
 	res = getIPAddress(dest);
 	addr = (struct sockaddr_in*)res.ai_addr;
+	skt = createRawSocket(addr);
+
 	printf("%u\n", addr->sin_addr.s_addr);
 
 	while (ping) {
-		wait(DELAY);
-		printf("X bytes from %s: icmp_seq:%d\n", inet_ntoa(addr->sin_addr), icmp_seq);
+		pkt = createPacket(pkt);
+		// printf("X bytes from %s: icmp_seq:%d\n", inet_ntoa(addr->sin_addr), icmp_seq);
+
+		clock_gettime(CLOCK_MONOTONIC, &t0);
+		sent = sendto(skt, pkt.buf, sizeof(pkt.buf), 0,
+		   (struct sockaddr*)addr, sizeof(*addr));
+		if (sent <= 0) {
+			fprintf(stderr, "Packet failed to send\n");
+		}
+
+		src_len = sizeof(source);
+
+		received = recvfrom(skt, pkt.buf, sizeof(pkt.buf), 0, 
+			&source, &src_len);
+		if (received <= 0) {
+			printf("Packet failed to recv\n");
+		}
+		else {
+			clock_gettime(CLOCK_MONOTONIC, &t1);
+			total_time = ((double)(t1.tv_nsec - t0.tv_nsec))/1000000.0;
+			rtt = ((t1.tv_sec - t0.tv_sec) * 1000.0) + total_time; 
+
+
+			if (pkt.buf[0] != 0) {
+				printf("wrong type of icmp pkt received\n");
+			}
+			else {
+				printf("echo reply received\n");
+			}
+
+		}
+
+		printf("received: %c\n", pkt.buf[0]);
 		icmp_seq += 1;
+		pkt.buf[0] = '\0';
+		wait(DELAY);
 		// fflush(stdout);
 	}
 	printf("\n--- %s ping statistics ---\n", dest);
@@ -83,6 +125,30 @@ void pingIPAddress(char *dest) {
 	// printf("protocol: %d\n", res.ai_protocol);
 	// printf("addr len: %d\n", res.ai_addrlen);
 	return;
+}
+
+int createRawSocket(struct sockaddr_in *addr) {
+	int skt = socket(addr->sin_family, SOCK_STREAM, 0); //IPPROTO_ICMP); 
+	if(skt < 0) { 
+		fprintf(stderr, "\nSocket file descriptor not received!!\n"); 
+		return 0; 
+	} 
+		printf("\nSocket file descriptor %d received\n", skt); 
+	return skt;
+}
+
+Packet createPacket(Packet pkt) {
+	pkt.type = ECHO_REQUEST;
+	pkt.code = TTL_EXPIRED;
+	pkt.checksum = 0;
+
+	memcpy(pkt.buf, &pkt.type, 1);
+	memcpy(&pkt.buf[1], &pkt.code, 1);
+	memcpy(&pkt.buf[2], &pkt.checksum, 2);
+
+	printf("%s\n", pkt.buf);
+
+	return pkt;
 }
 
 void wait(int seconds) {
--- ping.h
+++ ping.h
@@ -5,11 +5,29 @@
 
 #define DELAY 1
 
+#define ECHO_REPLY 0
+#define ECHO_REQUEST 8
+#define TTL_EXPIRED 11
+
+#define HEADER 4 //set a max value for the echo request pkt message
+
+typedef struct packet {
+	uint8_t type; // type of ICMP packet (echo reply/request, or neither)
+	uint8_t code; // additional type info (TTL expired, etc.)
+	uint16_t checksum; // checksum, will be set to 0
+
+	char buf[HEADER]; //the actual message to be sent
+
+}Packet; 
+
 void checkArgs(int argc, char *argv[]);
 
 struct addrinfo getIPAddress(char *dest);
 
 void pingIPAddress(char *dest);
+
+Packet createPacket(Packet pkt);
+int createRawSocket(struct sockaddr_in *addr);
 
 void wait(int ms);
 
